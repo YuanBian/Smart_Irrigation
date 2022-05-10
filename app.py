afae8a03a57b7e182de6c3d4feb03f46b52bb1fd
Yuan Bian <yuanbian95@gmail.com>
2022-05-10 16:58:53 -0500

    architectural changes

--- app.py
+++ app.py
@@ -11,14 +11,38 @@ WEATHER_API = os.getenv("WEATHER_API")
 
 app = Flask(__name__)
 
-@app.route("/weather", methods=["GET"])
-def GET_weather():
+# render frontend
+# @app.route("/", methods=["GET"])
+# def GET_watch():
+#     return 0
+
+# show current view of garden
+@app.route("/watch", methods=["GET"])
+def GET_watch():
     return 0
 
-@app.route("/alerts", methods=["GET"])
-def GET_alerts():
+# edit configs
+@app.route("/configs", methods=["GET"])
+def GET_change_configs():
     return 0
 
+# water now
+@app.route("/waterNow", methods=["GET"])
+def GET_waterNow():
+    return 0
+
+# use sensor now
+@app.route("/sensor", methods=["GET"])
+def GET_sensor():
+    return 0
+
+# get pi temperature
+@app.route("/pi_temperature", methods=["GET"])
+def GET_pi_temperature():
+    return 0
+
+# analysis
 @app.route("/analysis/<mode>", methods=["GET"])
 def GET_analysis(mode):
     return 0
+
--- garden_services.py
+++
@@ -1,9 +0,0 @@
-def open_valve(duration):
- return 0
-
-def time_lapse_footage():
- return 0
-
-def plant_analysis():
- return 0
-
