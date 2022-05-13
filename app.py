ecafde976a1d8e022378dd69542250908839dcb9
Tommy Ge <tommyge2@illinois.edu>
2022-05-13 22:33:03 +0100

    updating backend and middle ware

--- app.py
+++ app.py
@@ -4,51 +4,126 @@ import requests
 from datetime import datetime, timedelta, time
 import re
 from dotenv import load_dotenv
-import datetime
+from datetime import datetime
+import json
+from email.mime.text import MIMEText
+from email.mime.image import MIMEImage
+from email.mime.application import MIMEApplication
+from email.mime.multipart import MIMEMultipart
+import smtplib
+from apscheduler.schedulers.background import BackgroundScheduler
 
+# load weather API
 load_dotenv()
 WEATHER_API = os.getenv("WEATHER_API")
+APP_PASSWORD = os.getenv("APP_PASSWORD")
+def alerts_getter():
+    general_alerts = ""
+    frost_warning = ""
+    default_no_warning = general_alerts+frost_warning
+    weather_data = requests.get(WEATHER_API).json()
+    try: 
+        general_alerts = json.dumps(weather_data["alerts"])
+    except:
+        pass
+    min_temp = kelvinToCelsius(weather_data["daily"][1]["temp"]["min"]) # check temp for the next week
+    if min_temp<=2:
+        frost_warning = "There is frost tomorrow! "
+    msg_text = general_alerts+frost_warning
+    return msg_text
+
+def send_email():
+    print("hello")
+    alerts = alerts_getter()
+    if alerts !="":
+        smtp = smtplib.SMTP('smtp.gmail.com', 587)
+        smtp.ehlo()
+        smtp.starttls()
+        smtp.login('yuanbian95@gmail.com', APP_PASSWORD)
+        msg = MIMEMultipart()
+        msg['Subject'] = "Weather Warning"
+        msg.attach(MIMEText(alerts))
+        to = ["lxamatby@gmail.com"]
+        smtp.sendmail(from_addr="yuanbian95@gmail.com",
+                    to_addrs=to, msg=msg.as_string())
+        smtp.quit()
+
+sched = BackgroundScheduler()
+# sched.add_job(send_email,'cron',hour=2 , minute=31)
+sched.add_job(send_email,'cron',hour=8, minute=0, timezone="America/Chicago")
+sched.start()
+
 
 app = Flask(__name__)
 
 # render frontend
-# @app.route("/", methods=["GET"])
-# def GET_watch():
-#     return 0
+@app.route("/admin_page", methods=["GET"])
+def GET_admin_page():
+    return ""
+
+# render frontend
+@app.route("/analysis_page", methods=["GET"])
+def GET_analysis_page():
+    return ""
 
 # show current view of garden
 @app.route("/watch", methods=["GET"])
 def GET_watch():
-    return 0
+    return request.data
 
 # edit configs
-@app.route("/configs", methods=["GET"])
+@app.route("/configs", methods=["POST"])
 def GET_change_configs():
-    return 0
+    updated_configs = dict(request.form)
+    res = requests.post('http://127.0.0.1:5000/configs', data=json.dumps(updated_configs))
+    return res
 
 # water now
-@app.route("/waterNow", methods=["GET"])
+@app.route("/water_now", methods=["GET"])
 def GET_waterNow():
-    return 0
+    r = requests.get("http://127.0.0.1:5000/pi_temperature")
+    return r
 
 # use sensor now
-@app.route("/sensor", methods=["GET"])
+@app.route("/get_sensor_now", methods=["GET"])
 def GET_sensor():
+    r = requests.get("http://127.0.0.1:5000/pi_temperature").json()
     return 0
 
 # get pi temperature
 @app.route("/pi_temperature", methods=["GET"])
 def GET_pi_temperature():
-    return 0
+    r = requests.get("http://127.0.0.1:5000/pi_temperature").json()
+    return r["pi_temperature"]
+
+@app.route("/get_camera_now", methods=["GET"])
+def GET_camera():
+    now = datetime.now()
+    dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
+    img_path = "/home/pi/garden_services/smart_irrigation/photos/garden "+dt_string+".jpg"
+    r = requests.get("http://127.0.0.1:5000/pi_temperature")
+    print(r)
+    print(request.args)
+    return "img"
 
 # analysis
 @app.route("/analysis/<mode>", methods=["GET"])
 def GET_analysis(mode):
     return 0
 
+# analysis
+@app.route("/weather", methods=["GET"])
+def GET_weather():
+    weather_data = requests.get(WEATHER_API).json()
+    return json.dumps(weather_data["daily"][0])
+
+def kelvinToCelsius(kelvin):
+    return kelvin - 273.15
+
 # get alerts from weather api
 # additional feature: send "Frost Warning" alert when the next day's average temperature drops below 2 degree Celsius
 @app.route("/alerts", methods=["GET"])
 def GET_alerts():
-    return 0
+    alerts = alerts_getter()
+    return alerts, 200
 
--- backend/app.py
+++ backend/app.py
@@ -1,70 +1,123 @@
-from flask import Flask, render_template, request, jsonify
+# RUNNING ON PORT 5000
+from flask import Flask, render_template, request, jsonify, send_file
 import os
 import requests
 from datetime import datetime, timedelta, time
+import time as systi
 import re
 from dotenv import load_dotenv
 import datetime
 import json
-
-load_dotenv()
-WEATHER_API = os.getenv("WEATHER_API")
-
-#  1. change configs
-#  2. use sensors
-#  3. open valve
-#  4. measure Pi internal temperature
+import RPi.GPIO as GPIO
+import atexit
+from apscheduler.schedulers.background import BackgroundScheduler
 
 app = Flask(__name__)
 
-app.config.update(
-  valveTime= 10,
-  photoTimeInterval= 60,
-  moistureSensorCheckTime= 60,
-  emails= ["yuanbian95@gmail.com", "mklimuszka@gmail.com"],
-  frostWarningThreshold= 2
-)
+app.config.from_file("config.json", load=json.load)
 
-@app.route("/configs/<value>", methods=["GET"])
-def GET_change_configs(value):
-  app.config["valveTime"] = "WITH SOME VALUES"
-  return 0
+# set up pi GPIO
+GPIO.setmode(GPIO.BCM)
+# input_pin = 2
+# GPIO.setup(input_pin, GPIO.IN)
+
+
+# load env
+load_dotenv()
+WEATHER_API = os.getenv("WEATHER_API")
+
+def setup_pins(pins, GPIO_TYPE):
+  for pin in pins:
+    GPIO.setup(pin, GPIO_TYPE)
+
+valve_pins = app.config["VALVE_OUTPUTS"]
+setup_pins(valve_pins, GPIO.OUT)
+# input for sensors
+sensors_pins = app.config["SENSOR_INPUTS"]
+setup_pins(sensors_pins, GPIO.IN)
+
+def camera_schedule():
+  img_path = "/home/pi/garden_services/smart_irrigation/photos/garden.jpg"
+  os.system("raspistill -o "+img_path)
+  # img = send_file(img_path, mimetype='image/gif')
+  # os.system("rm "+img_path)
+  # return img
+
+def open_valve():
+  GPIO.output(valve_pins[0], GPIO.HIGH)
+  systi.sleep(10)
+  GPIO.output(valve_pins[0], GPIO.LOW)
+  return "Watered!"
+
+def water_schedule():
+  weather_data = requests.get(
+    WEATHER_API
+  ).json()
+  today_data = weather_data["daily"][0]
+  rain_chance = today_data["pop"]
+  rain_level = None
+  try:
+      rain_level = today_data["rain"]
+  except:
+      pass
+  if rain_level:
+      open_valve()
+
+
+sched = BackgroundScheduler(daemon=True)
+sched.add_job(water_schedule,'cron',hour='8, 18', minute=0, timezone="America/Chicago")
+# sched.add_job(water_schedule,'interval',minutes=60)
+sched.add_job(camera_schedule,'interval',minutes=60)
+
+sched.start()
+
+
+def exit():
+  GPIO.cleanup()
+  print("GPIO pins are cleaned up!")
+
+atexit.register(exit)
+
+@app.route("/configs", methods=["POST"])
+def GET_change_configs():
+  updated_configs = request.get_json(force=True)
+  print(updated_configs)
+  f = open("config.json", "w")
+  f.write(json.dumps(updated_configs, indent=4))
+  f.close()
+  return request
 
 @app.route("/sensor", methods=["GET"])
 def GET_sensor():
-  return 0
+  input_list = []
+  for pin in sensors_pins:
+    val = GPIO.input(pin)
+    input_list.append(val)
+  return str(input_list)
 
-# @app.route("/water", methods=["GET"])
-# def GET_water():
-#   return 0
+@app.route("/water", methods=["GET"])
+def GET_water():
+  msg = ""
+  try:
+    valve_pins = app.config["VALVE_OUTPUTS"]
+    setup_pins(valve_pins, GPIO.OUT)
+    # GPIO.output(valve_pins[0], GPIO.HIGH)
+    msg = "Success!"
+  except:
+    msg = "Failed!"
+  return msg
+  
+@app.route("/pi_temperature", methods=["GET"])
+def GET_pi_temperature():
+    temperature = os.popen('vcgencmd measure_temp').read()
+    temp_int = temperature[5:-3]
+    temp_json = json.dumps({"pi_temperature": temp_int})
+    return temp_json
 
-# @app.route("/pi_temperature", methods=["GET"])
-# def GET_pi_temperature():
-#   return 0
-
-# @app.route("/change_config/<value>", methods=["GET"])
-# def GET_weather():
-#  config_f = open("/Users/yuanbian/Desktop/smart_irrigation/backend/config.json","r")
-#  configs = json.load(config_f)
-#  config_f.close()
-
-#  valveTime = configs["valveTime"]
-#  photoTimeInterval = configs["photoTimeInterval"]
-#  moistureSensorCheckTime = configs["moistureSensorCheckTime"]
-#  frostWarningThreshold = configs["frostWarningThreshold"]
-#  emails = configs["emails"]
-
-#  new_configs = {
-#    "valveTime": valveTime,
-#    "photoTimeInterval": photoTimeInterval,
-#    "moistureSensorCheckTime": moistureSensorCheckTime,
-#    "emails":emails,
-#    "frostWarningThreshold": frostWarningThreshold
-#  }
-
-#  new_configs_str = json.dumps(new_configs)
-#  new_config_f = open("/Users/yuanbian/Desktop/smart_irrigation/backend/config.json","w")
-#  new_config_f.write(new_configs_str)
-#  new_config_f.close()
-
-#  return "current configs: "+new_configs_str
+@app.route("/camera", methods=["GET"])
+def GET_camera():
+  img_path = "/home/pi/garden_services/smart_irrigation/photos/garden.jpg"
+  os.system("raspistill -o "+img_path)
+  img = send_file(img_path, mimetype='image/gif')
+  os.system("rm "+img_path)
+  return img
