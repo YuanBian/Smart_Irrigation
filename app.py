ca5a014e4e2c713eae6c01a1a0ffb06a6626dfdd
Yuan Bian <ybian3@illinois.edu>
2023-07-13 18:36:58 -0500

    Better documentation

--- app.py
+++ app.py
@@ -17,40 +17,47 @@ from apscheduler.schedulers.background import BackgroundScheduler
 load_dotenv()
 WEATHER_API = os.getenv("WEATHER_API")
 APP_PASSWORD = os.getenv("APP_PASSWORD")
+FROM_EMAIL = os.getenv("FROM_EMAIL")
+TO_EMAIL = os.getenv("TO_EMAIL")
+
+
 def alerts_getter():
+    """Return a warning message if any anomaly is detected"""
     general_alerts = ""
     frost_warning = ""
-    default_no_warning = general_alerts+frost_warning
+    default_no_warning = general_alerts + frost_warning
     weather_data = requests.get(WEATHER_API).json()
-    try: 
+    try:
         general_alerts = json.dumps(weather_data["alerts"])
     except:
         pass
-    min_temp = kelvinToCelsius(weather_data["daily"][1]["temp"]["min"]) # check temp for the next week
-    if min_temp<=2:
+    # check temp for the next week
+    min_temp = kelvinToCelsius(weather_data["daily"][1]["temp"]["min"])
+    if min_temp <= 2:
         frost_warning = "There is frost tomorrow! "
-    msg_text = general_alerts+frost_warning
+    msg_text = general_alerts + frost_warning
     return msg_text
 
+
 def send_email():
-    print("hello")
+    """Send an email if there is an alert"""
     alerts = alerts_getter()
-    if alerts !="":
-        smtp = smtplib.SMTP('smtp.gmail.com', 587)
+    if alerts != "":
+        smtp = smtplib.SMTP("smtp.gmail.com", 587)
         smtp.ehlo()
         smtp.starttls()
-        smtp.login('yuanbian95@gmail.com', APP_PASSWORD)
+        smtp.login(FROM_EMAIL, APP_PASSWORD)
         msg = MIMEMultipart()
-        msg['Subject'] = "Weather Warning"
+        msg["Subject"] = "Weather Warning"
         msg.attach(MIMEText(alerts))
-        to = ["mklimuszka@gmail.com"]
-        smtp.sendmail(from_addr="yuanbian95@gmail.com",
-                    to_addrs=to, msg=msg.as_string())
+        to = [TO_EMAIL]
+        smtp.sendmail(from_addr=FROM_EMAIL, to_addrs=to, msg=msg.as_string())
         smtp.quit()
 
+
+# schedule a job to send email at 8 am every morning
 sched = BackgroundScheduler()
-# sched.add_job(send_email,'cron',hour=2 , minute=31)
-sched.add_job(send_email,'cron',hour=8, minute=0, timezone="America/Chicago")
+sched.add_job(send_email, "cron", hour=8, minute=0, timezone="America/Chicago")
 sched.start()
 
 
@@ -61,35 +68,43 @@ app = Flask(__name__)
 def GET_admin_page():
     return render_template("admin_page.html")
 
+
 # show current view of garden
 @app.route("/watch", methods=["GET"])
 def GET_watch():
     return request.data
+
 
 # edit configs
 @app.route("/configs", methods=["POST"])
 def GET_change_configs():
     updated_configs = dict(request.form)
     print(updated_configs)
-    res = requests.post('http://127.0.0.1:5000/configs', data=json.dumps(updated_configs))
-    return "Status code: "+str(res.status_code)
+    res = requests.post(
+        "http://127.0.0.1:5000/configs", data=json.dumps(updated_configs)
+    )
+    return "Status code: " + str(res.status_code)
+
 
 # water now
 @app.route("/water_now", methods=["GET"])
 def GET_waterNow():
     res = requests.get("http://192.168.1.38:5000/water")
-    return "Status code: "+str(res.status_code)
+    return "Status code: " + str(res.status_code)
+
 
 @app.route("/stop_water", methods=["GET"])
 def GET_stop_water():
     res = requests.get("http://192.168.1.38:5000/stop_water")
-    return "Status code: "+str(res.status_code)
-  
+    return "Status code: " + str(res.status_code)
+
+
 # use sensor now
 @app.route("/get_sensor_now", methods=["GET"])
 def GET_sensor():
     res = requests.get("http://127.0.0.1:5000/sensor").json()
-    return "Sensor data: "+str(res)
+    return "Sensor data: " + str(res)
+
 
 # get pi temperature
 @app.route("/pi_temperature", methods=["GET"])
@@ -97,29 +112,16 @@ def GET_pi_temperature():
     r = requests.get("http://127.0.0.1:5000/pi_temperature").json()
     return r["pi_temperature"]
 
-# get pi temperature
-# @app.route("/get_chart", methods=["GET"])
-# def GET_chart():
-#     r = requests.get("http://127.0.0.1:5000/chart").json()
-#     return 
-
-# @app.route("/get_camera_now", methods=["GET"])
-# def GET_camera():
-#     now = datetime.now()
-#     dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
-#     img_path = "/home/pi/garden_services/smart_irrigation/photos/garden "+dt_string+".jpg"
-#     r = requests.get("http://127.0.0.1:5000/pi_temperature")
-#     print(r)
-#     print(request.args)
-#     return "img"
 
 @app.route("/weather", methods=["GET"])
 def GET_weather():
     weather_data = requests.get(WEATHER_API).json()
     return json.dumps(weather_data["daily"][0])
 
+
 def kelvinToCelsius(kelvin):
     return kelvin - 273.15
+
 
 # get alerts from weather api
 # additional feature: send "Frost Warning" alert when the next day's average temperature drops below 2 degree Celsius
@@ -127,4 +129,3 @@ def kelvinToCelsius(kelvin):
 def GET_alerts():
     alerts = alerts_getter()
     return alerts, 200
-
--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,3 @@
-# RUNNING ON PORT 5000
 from flask import Flask, render_template, request, jsonify, send_file
 import os
 import requests
@@ -18,187 +17,184 @@ app.config.from_file("config.json", load=json.load)
 
 # set up pi GPIO
 GPIO.setmode(GPIO.BCM)
-# input_pin = 2
-# GPIO.setup(input_pin, GPIO.IN)
 
-
-# load env
 load_dotenv()
 WEATHER_API = os.getenv("WEATHER_API")
+IMG_PATH = os.getenv("IMG_PATH")
+
 
 def setup_pins(pins, GPIO_TYPE):
-  for pin in pins:
-    GPIO.setup(pin, GPIO_TYPE)
+    for pin in pins:
+        GPIO.setup(pin, GPIO_TYPE)
 
-# valve_pins = app.config["VALVE_OUTPUTS"]
-# setup_pins(valve_pins, GPIO.OUT)
-# # input for sensors
-# sensors_pins = app.config["SENSOR_INPUTS"]
-# setup_pins(sensors_pins, GPIO.IN)
 
 def camera_schedule():
-  img_path = "/home/pi/garden_services/smart_irrigation/photos/garden.jpg"
-  os.system("raspistill -o "+img_path)
-  # img = send_file(img_path, mimetype='image/gif')
-  # os.system("rm "+img_path)
-  # return img
+    os.system("raspistill -o " + IMG_PATH)
+
 
 def open_valve():
-  valve_pins = app.config["VALVE_OUTPUTS"]
-  setup_pins(valve_pins, GPIO.OUT)
-  GPIO.output(valve_pins[0], GPIO.HIGH)
-  systi.sleep(60*app.config["VALVE_TIME"])
-  GPIO.output(valve_pins[0], GPIO.LOW)
-  return "Watered!"
+
+    valve_pins = app.config["VALVE_OUTPUTS"]
+    setup_pins(valve_pins, GPIO.OUT)
+    GPIO.output(valve_pins[0], GPIO.HIGH)
+    systi.sleep(60 * app.config["VALVE_TIME"])
+    GPIO.output(valve_pins[0], GPIO.LOW)
+    return "Watered!"
+
 
 def get_current_time():
-  now = datetime.now()
-  dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
-  return dt_string
+    now = datetime.now()
+    dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
+    return dt_string
+
 
 def water_schedule():
-  sensors_pins = app.config["SENSOR_INPUTS"]
-  setup_pins(sensors_pins, GPIO.IN)
-  weather_data = requests.get(
-    WEATHER_API
-  ).json()
-  today_data = weather_data["daily"][0]
-  rain_chance = today_data["pop"]
-  rain_level = None
-  try:
-      rain_level = today_data["rain"]
-  except:
-      pass
-  
-  # ctr = 0
-  # for p in sensors_pins:
-  #     # if soil is dry
-  #     if GPIO.input(p):
-  #         ctr += 1
-  if not rain_level:# and ctr >= len(sensors_pins) /  2:
-      with open(f'water_schedule.txt', 'a') as f:
-        f.write(get_current_time+": Running water schedule for "+str(app.config["VALVE_TIME"])+" minutes!")
-      print("Running water schedule!")
-      open_valve()
+    sensors_pins = app.config["SENSOR_INPUTS"]
+    setup_pins(sensors_pins, GPIO.IN)
+    weather_data = requests.get(WEATHER_API).json()
+    today_data = weather_data["daily"][0]
+    rain_chance = today_data["pop"]
+    rain_level = None
+    try:
+        rain_level = today_data["rain"]
+    except:
+        pass
+
+    if not rain_level:
+        with open(f"water_schedule.txt", "a") as f:
+            f.write(
+                get_current_time
+                + ": Running water schedule for "
+                + str(app.config["VALVE_TIME"])
+                + " minutes!"
+            )
+        print("Running water schedule!")
+        open_valve()
+
 
 def get_temperature():
-    temperature = os.popen('vcgencmd measure_temp').read()
+    temperature = os.popen("vcgencmd measure_temp").read()
     temp_int = temperature[5:-3]
     return temp_int
 
+
 def temperature_job():
-    with open(f'temperature.txt', 'a') as f:
-        f.write(get_temperature() + '\n')
+    with open(f"temperature.txt", "a") as f:
+        f.write(get_temperature() + "\n")
+
 
 sched = BackgroundScheduler(daemon=True)
-#sched.add_job(water_schedule,'cron',hour='8, 18', minute=0, timezone="America/Chicago")
-sched.add_job(water_schedule,'cron',hour="8,18", minute=0, timezone="America/Chicago")
-# sched.add_job(camera_schedule,'interval',minutes=60)
-sched.add_job(temperature_job, 'interval', seconds=60)
+sched.add_job(water_schedule, "cron", hour="8,18", minute=0, timezone="America/Chicago")
+sched.add_job(temperature_job, "interval", seconds=60)
 sched.start()
 
 
 def exit_cleanup():
-  GPIO.cleanup()
-  print("GPIO pins are cleaned up!")
+    GPIO.cleanup()
+    print("GPIO pins are cleaned up!")
+
 
 atexit.register(exit_cleanup)
 
+
 @app.route("/configs", methods=["POST"])
 def GET_change_configs():
-  updated_configs_raw = request.get_data().decode("utf-8")
-  updated_configs = json.loads(updated_configs_raw, parse_int=int)
-  for key in updated_configs:
-    # print(key+": "+updated_configs[key])
-    if updated_configs[key]!="":
-      print()
-      if key=="SENSOR_INPUTS" or key=="VALVE_OUTPUTS":
-        updated_configs[key] = [int(x) for x in updated_configs[key].split(" ")]
-        print(updated_configs[key])
-      else:
-        updated_configs[key]= int(updated_configs[key])
-    else:
-      if key=="SENSOR_INPUTS" or key=="VALVE_OUTPUTS":
-        updated_configs[key]= app.config[key]
-      else:
-        updated_configs[key]= int(app.config[key])
-      print(type(updated_configs[key]))
-  f = open("config.json", "w")
-  f.write(json.dumps(updated_configs))
-  f.close()
-  return "success"
+    updated_configs_raw = request.get_data().decode("utf-8")
+    updated_configs = json.loads(updated_configs_raw, parse_int=int)
+    for key in updated_configs:
+        if updated_configs[key] != "":
+            print()
+            if key == "SENSOR_INPUTS" or key == "VALVE_OUTPUTS":
+                updated_configs[key] = [int(x) for x in updated_configs[key].split(" ")]
+                print(updated_configs[key])
+            else:
+                updated_configs[key] = int(updated_configs[key])
+        else:
+            if key == "SENSOR_INPUTS" or key == "VALVE_OUTPUTS":
+                updated_configs[key] = app.config[key]
+            else:
+                updated_configs[key] = int(app.config[key])
+            print(type(updated_configs[key]))
+    f = open("config.json", "w")
+    f.write(json.dumps(updated_configs))
+    f.close()
+    return "success"
+
 
 @app.route("/sensor", methods=["GET"])
 def GET_sensor():
-  sensors_pins = app.config["SENSOR_INPUTS"]
-  setup_pins(sensors_pins, GPIO.IN)
-  input_list = []
-  for pin in sensors_pins:
-    val = GPIO.input(pin)
-    input_list.append(val)
-  return str(input_list), 200
+    sensors_pins = app.config["SENSOR_INPUTS"]
+    setup_pins(sensors_pins, GPIO.IN)
+    input_list = []
+    for pin in sensors_pins:
+        val = GPIO.input(pin)
+        input_list.append(val)
+    return str(input_list), 200
+
 
 @app.route("/water", methods=["GET"])
 def GET_water():
-  msg = ""
-  code = 200
-  try:
-    # valve_pins = app.config["VALVE_OUTPUTS"]
-    # setup_pins(valve_pins, GPIO.OUT)
-    # GPIO.output(valve_pins[0], GPIO.HIGH)
-    # systi.sleep(10)
-    open_valve()
-    print("WATERING NOW!")
-    msg = "Success! Watering Now!"
-  except:
-    msg = "Failed!"
-    code = 500
-  print(msg)
-  return msg, code
+    msg = ""
+    code = 200
+    try:
+        open_valve()
+        print("WATERING NOW!")
+        msg = "Success! Watering Now!"
+    except:
+        msg = "Failed!"
+        code = 500
+    print(msg)
+    return msg, code
+
 
 @app.route("/stop_water", methods=["GET"])
 def GET_stop_water():
-  msg = ""
-  try:
-    valve_pins = app.config["VALVE_OUTPUTS"]
-    setup_pins(valve_pins, GPIO.OUT)
-    GPIO.output(valve_pins[0], GPIO.LOW)
-    msg = "Success! Water stopped"
-  except:
-    msg = "Failed!"
-  print(msg)
-  return msg
-  
+    msg = ""
+    try:
+        valve_pins = app.config["VALVE_OUTPUTS"]
+        setup_pins(valve_pins, GPIO.OUT)
+        GPIO.output(valve_pins[0], GPIO.LOW)
+        msg = "Success! Water stopped"
+    except:
+        msg = "Failed!"
+    print(msg)
+    return msg
+
+
 @app.route("/pi_temperature", methods=["GET"])
 def GET_pi_temperature():
     temp_int = get_temperature()
     temp_json = json.dumps({"pi_temperature": temp_int})
     return temp_json
 
-# @app.route("/temp_analysis", methods=['GET'])
+
 def temp_analysis():
     temps = []
-    with open('temperature.txt', "r") as f:
+    with open("temperature.txt", "r") as f:
         for l in f:
             temps.append(float(l))
     x = [i for i in range(len(temps))]
     plt.plot(x, temps)
-    plt.xlabel('Time Stamp')
-    plt.ylabel('pi temperature')
-    plt.title('Temperature of Raspberry Pi Over Time')
-    plt.savefig('temperature_chart.png')
+    plt.xlabel("Time Stamp")
+    plt.ylabel("pi temperature")
+    plt.title("Temperature of Raspberry Pi Over Time")
+    plt.savefig("temperature_chart.png")
+
 
 @app.route("/camera", methods=["GET"])
 def GET_camera():
-  img_path = "/home/pi/garden_services/smart_irrigation/photos/garden.jpg"
-  os.system("raspistill -o "+img_path)
-  img = send_file(img_path, mimetype='image/gif')
-  os.system("rm "+img_path)
-  return img, 200
+    img_path = "/home/pi/garden_services/smart_irrigation/photos/garden.jpg"
+    os.system("raspistill -o " + img_path)
+    img = send_file(img_path, mimetype="image/gif")
+    os.system("rm " + img_path)
+    return img, 200
 
-@app.route('/chart', methods=["GET"])
+
+@app.route("/chart", methods=["GET"])
 def show_chart():
     temp_analysis()
-    full_filename = "/home/pi/garden_services/smart_irrigation/backend/temperature_chart.png"
-    img = send_file(full_filename, mimetype='image/gif')
+    full_filename = (
+        "/home/pi/garden_services/smart_irrigation/backend/temperature_chart.png"
+    )
+    img = send_file(full_filename, mimetype="image/gif")
     return img, 200
